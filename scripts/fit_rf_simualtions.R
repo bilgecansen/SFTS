fd868d87d6558d4addaa27503715c59b848fe2ef
Bilgecan Sen <bilgecan.sen@gmail.com>
2025-05-14 12:07:18 -0400

    add new quadratic scenario and simplify code

--- scripts/fit_rf_simualtions.R
+++ scripts/fit_rf_simualtions.R
@@ -4,6 +4,8 @@ library(foreach)
 library(patchwork)
 library(truncnorm)
 
+theme_set(theme_bw())
+
 
 # Univariate Relationships ------------------------------------------------
 
@@ -121,7 +123,7 @@ simulate_ab <- function(response, t_x = 0:19) {
     a <- 1000
     b <- 10
     mu_sx <- apply(m_x, 2, mean)
-    mu_ab <- a + b * mu_x
+    mu_ab <- a + b * mu_sx
 
     b2 <- rnorm(20, 10, 10)
     mu_tx <- apply(m_x, 1, mean)
@@ -130,6 +132,28 @@ simulate_ab <- function(response, t_x = 0:19) {
       {
         mu_ab + b2 * mu_tx[i] #+ site_effects
       }
+
+    dat_ab <- data.frame(m_ab)
+    colnames(dat_ab) <- 1:20
+    dat_ab$site <- 1:20
+
+    dat_ab <- pivot_longer(
+      dat_ab,
+      !site,
+      names_to = "time",
+      values_to = "abundance"
+    )
+
+    dat_raw <- left_join(dat_ab, dat_x, by = c("site", "time"))
+  }
+
+  if (response == "quadratic") {
+    a <- 1000
+    b <- 40
+    c <- -1
+    mu_ab <- a + b * m_x + c * m_x^2
+
+    m_ab <- apply(mu_ab, c(1, 2), function(x) rnorm(1, x, 10))
 
     dat_ab <- data.frame(m_ab)
     colnames(dat_ab) <- 1:20
@@ -194,23 +218,26 @@ simulate_ab <- function(response, t_x = 0:19) {
   list(raw = dat_raw, dec = dat_dec)
 }
 
-dat_sp1 <- simulate_ab(response = "spatial")
-dat_temp1 <- simulate_ab(response = "temporal-fixed")
-dat_sptemp1 <- simulate_ab(response = "spatio-temporal")
-dat_tempvar1 <- simulate_ab(response = "temporal-variable")
-dat_sptempvar1 <- simulate_ab(response = "spatial + temporal-variable")
+scenarios <- c(
+  "spatial",
+  "temporal-fixed",
+  "spatio-temporal",
+  "temporal-variable",
+  "spatial + temporal-variable",
+  "quadratic"
+)
 
-dat_sp2 <- simulate_ab(response = "spatial", t_x = seq(0, 9.5, 0.5))
-dat_temp2 <- simulate_ab(response = "temporal-fixed", t_x = seq(0, 9.5, 0.5))
-dat_sptemp2 <- simulate_ab(response = "spatio-temporal", t_x = seq(0, 9.5, 0.5))
-dat_tempvar2 <- simulate_ab(
-  response = "temporal-variable",
-  t_x = seq(0, 9.5, 0.5)
-)
-dat_sptempvar2 <- simulate_ab(
-  response = "spatial + temporal-variable",
-  t_x = seq(0, 9.5, 0.5)
-)
+dat_equal <- foreach(i = 1:length(scenarios)) %do%
+  {
+    simulate_ab(response = scenarios[i])
+  }
+names(dat_equal) <- scenarios
+
+dat_uneven <- foreach(i = 1:length(scenarios)) %do%
+  {
+    simulate_ab(response = scenarios[i], t_x = seq(0, 9.5, 0.5))
+  }
+names(dat_uneven) <- scenarios
 
 plot_univ <- function(dat) {
   ggplot(data = dat) +
@@ -245,197 +272,60 @@ plot_univ <- function(dat) {
     )
 }
 
-gm1 <- plot_univ(dat_sp1$raw)
-gm2 <- plot_univ(dat_temp1$raw)
-gm3 <- plot_univ(dat_sptemp1$raw)
-gm4 <- plot_univ(dat_tempvar1$raw)
-gm5 <- plot_univ(dat_sptempvar1$raw)
+gm_equal <- foreach(i = 1:length(dat_equal)) %do%
+  {
+    plot_univ(dat_equal[[i]]$raw)
+  }
 
-gm1 +
-  gm2 +
-  gm3 +
-  gm4 +
-  plot_layout(axes = "collect") +
-  plot_annotation(tag_levels = "a")
+gm_uneven <- foreach(i = 1:length(dat_equal)) %do%
+  {
+    plot_univ(dat_uneven[[i]]$raw)
+  }
 
-#ggsave("fig1.pdf", width = 180, height = 90, units = "mm", dpi = 600)
-
-gm5 <- plot_univ(dat_sp2$raw)
-gm6 <- plot_univ(dat_temp2$raw)
-gm7 <- plot_univ(dat_sptemp2$raw)
-gm8 <- plot_univ(dat_tempvar2$raw)
-gm9 <- plot_univ(dat_sptempvar2$raw)
-
-gm5 +
-  gm6 +
-  gm7 +
-  gm8 +
-  plot_layout(axes = "collect") +
-  plot_annotation(tag_levels = "a")
-
-#ggsave("fig1.pdf", width = 180, height = 90, units = "mm", dpi = 600)
 
 # Variable Importance -----------------------------------------------------
 
-res_sim_sp1 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_sp1$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
+res_equal <- foreach(i = 1:length(dat_equal)) %do%
+  {
+    ranger(
+      abundance ~ .,
+      data = select(
+        dat_equal[[i]]$dec,
+        -site,
+        -time,
+        -env,
+        -env_rand1,
+        -env_rand2,
+        -env_rand3
+      ),
+      importance = "permutation",
+      mtry = 10,
+      num.trees = 2000
+    )
+  }
+names(res_equal) <- scenarios
 
-res_sim_sp2 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_sp2$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
+res_uneven <- foreach(i = 1:length(dat_uneven)) %do%
+  {
+    ranger(
+      abundance ~ .,
+      data = select(
+        dat_uneven[[i]]$dec,
+        -site,
+        -time,
+        -env,
+        -env_rand1,
+        -env_rand2,
+        -env_rand3
+      ),
+      importance = "permutation",
+      mtry = 10,
+      num.trees = 2000
+    )
+  }
+names(res_uneven) <- scenarios
 
-res_sim_temp1 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_temp1$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
-
-res_sim_temp2 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_temp2$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
-
-res_sim_sptemp1 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_sptemp1$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
-
-res_sim_sptemp2 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_sptemp$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
-
-res_sim_tempvar1 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_tempvar2$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
-
-res_sim_tempvar2 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_tempvar2$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
-
-res_sim_tempvar1 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_tempvar2$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
-
-res_sim_tempvar2 <- ranger(
-  abundance ~ .,
-  data = select(
-    dat_tempvar2$dec,
-    -site,
-    -time,
-    -env,
-    -env_rand1,
-    -env_rand2,
-    -env_rand3
-  ),
-  importance = "permutation",
-  mtry = 10,
-  num.trees = 2000
-)
+# Plots
 plot_imp <- function(res, title) {
   var_imp <- res$variable.importance
   var_imp <- var_imp[order(var_imp, decreasing = T)][1:12]
@@ -466,175 +356,94 @@ plot_imp <- function(res, title) {
     )
 }
 
-plot_imp(res_sim_sp1, title = "Spatial Response")
-plot_imp(res_sim_temp1, title = "Fixed Temporal Response")
-plot_imp(res_sim_sptemp1, title = "Spatio-Temporal Response")
-plot_imp(res_sim_tempvar1, title = "Variable Temporal Response")
+titles <- c(
+  "Spatial Response",
+  "Fixed Temporal Response",
+  "Spatio-Temporal Response",
+  "Variable Temporal Response",
+  "Spatial + Variable Temporal Response",
+  "Quadratic Response"
+)
 
-plot_imp(res_sim_sp2, title = "Spatial Response")
-plot_imp(res_sim_temp2, title = "Fixed Temporal Response")
-plot_imp(res_sim_sptemp2, title = "Spatio-Temporal Response")
-plot_imp(res_sim_tempvar2, title = "Variable Temporal Response")
+g_imp_equal <- foreach(i = 1:length(res_equal)) %do%
+  {
+    plot_imp(res_equal[[i]], title = titles[i])
+  }
+
+g_imp_uneven <- foreach(i = 1:length(res_uneven)) %do%
+  {
+    plot_imp(res_uneven[[i]], title = titles[i])
+  }
 
 
 # Test set predictions ----------------------------------------------------
 
-# Spatial
-res_sim_sp1 <- ranger(
-  abundance ~
-    env_spatial + env_rand1_spatial + env_rand2_spatial + env_rand3_spatial,
-  data = dat_sp1$dec,
-  mtry = 3,
-  num.trees = 2000
-)
+# Models with only spatial components
+res_test_equal <- foreach(i = 1:length(dat_equal)) %do%
+  {
+    ranger(
+      abundance ~
+        env_spatial + env_rand1_spatial + env_rand2_spatial + env_rand3_spatial,
+      data = dat_equal[[i]]$dec,
+      mtry = 3,
+      num.trees = 2000
+    )
+  }
 
-dat_sp_test1 <- simulate_ab(response = "spatial")
-colnames(dat_sp_test1$raw)[-(1:3)] <- paste(
-  colnames(dat_sp_test1$raw)[-(1:3)],
-  "spatial",
-  sep = "_"
-)
-pred_sim1 <- predict(res_sim_sp1, data = dat_sp_test1$raw)
-dat_sp_test1$raw$pred <- pred_sim1$predictions
+res_test_uneven <- foreach(i = 1:length(dat_equal)) %do%
+  {
+    ranger(
+      abundance ~
+        env_spatial + env_rand1_spatial + env_rand2_spatial + env_rand3_spatial,
+      data = dat_uneven[[i]]$dec,
+      mtry = 3,
+      num.trees = 2000
+    )
+  }
 
-res_sim_sp2 <- ranger(
-  abundance ~
-    env_spatial + env_rand1_spatial + env_rand2_spatial + env_rand3_spatial,
-  data = dat_sp2$dec,
-  mtry = 3,
-  num.trees = 2000
-)
+# Simulate new test data
+dat_test_equal <- foreach(i = 1:length(scenarios)) %do%
+  {
+    z <- simulate_ab(response = scenarios[i])
+    colnames(z$raw)[-(1:3)] <- paste(
+      colnames(z$raw)[-(1:3)],
+      "spatial",
+      sep = "_"
+    )
 
-dat_sp_test2 <- simulate_ab(response = "spatial", t_x = seq(0, 9.5, 0.5))
-colnames(dat_sp_test2$raw)[-(1:3)] <- paste(
-  colnames(dat_sp_test2$raw)[-(1:3)],
-  "spatial",
-  sep = "_"
-)
-pred_sim2 <- predict(res_sim_sp2, data = dat_sp_test2$raw)
-dat_sp_test2$raw$pred <- pred_sim2$predictions
+    z
+  }
+names(dat_test_equal) <- scenarios
 
-# Fixed Temporal
-res_sim_temp1 <- ranger(
-  abundance ~
-    env_temporal + env_rand1_temporal + env_rand2_temporal + env_rand3_temporal,
-  data = dat_temp1$dec,
-  mtry = 3,
-  num.trees = 2000
-)
+dat_test_uneven <- foreach(i = 1:length(scenarios)) %do%
+  {
+    z <- simulate_ab(response = scenarios[i], t_x = seq(0, 9.5, 0.5))
+    colnames(z$raw)[-(1:3)] <- paste(
+      colnames(z$raw)[-(1:3)],
+      "spatial",
+      sep = "_"
+    )
 
-dat_temp_test1 <- simulate_ab(response = "temporal-fixed")
-colnames(dat_temp_test1$raw)[-(1:3)] <- paste(
-  colnames(dat_temp_test1$raw)[-(1:3)],
-  "temporal",
-  sep = "_"
-)
-pred_sim1 <- predict(res_sim_temp1, data = dat_temp_test1$raw)
-dat_temp_test1$raw$pred <- pred_sim1$predictions
+    z
+  }
+names(dat_test_uneven) <- scenarios
 
-res_sim_temp2 <- ranger(
-  abundance ~
-    env_temporal + env_rand1_temporal + env_rand2_temporal + env_rand3_temporal,
-  data = dat_temp2$dec,
-  mtry = 3,
-  num.trees = 2000
-)
+# predict test data
+pred_equal <- foreach(i = 1:length(res_test_equal)) %do%
+  {
+    predict(res_test_equal[[i]], data = dat_test_equal[[i]]$raw)
+  }
+for (i in 1:length(dat_test_equal)) {
+  dat_test_equal[[i]]$raw$pred <- pred_equal[[i]]$predictions
+}
 
-dat_temp_test2 <- simulate_ab(
-  response = "temporal-fixed",
-  t_x = seq(0, 9.5, 0.5)
-)
-colnames(dat_temp_test2$raw)[-(1:3)] <- paste(
-  colnames(dat_temp_test2$raw)[-(1:3)],
-  "temporal",
-  sep = "_"
-)
-pred_sim2 <- predict(res_sim_temp2, data = dat_temp_test2$raw)
-dat_temp_test2$raw$pred <- pred_sim2$predictions
-
-# Spatio-temporal
-res_sim_sptemp1 <- ranger(
-  abundance ~
-    env_temporal +
-      env_rand1_temporal +
-      env_rand2_temporal +
-      env_rand3_temporal +
-      env_spatial +
-      env_rand1_spatial +
-      env_rand2_spatial +
-      env_rand3_spatial,
-  data = dat_sptemp1$dec,
-  mtry = 3,
-  num.trees = 2000
-)
-
-dat_sptemp_test1 <- simulate_ab(response = "spatio-temporal")
-pred_sim1 <- predict(res_sim_sptemp1, data = dat_sptemp_test1$dec)
-dat_sptemp_test1$dec$pred <- pred_sim1$predictions
-
-res_sim_sptemp2 <- ranger(
-  abundance ~
-    env_temporal +
-      env_rand1_temporal +
-      env_rand2_temporal +
-      env_rand3_temporal +
-      env_spatial +
-      env_rand1_spatial +
-      env_rand2_spatial +
-      env_rand3_spatial,
-  data = dat_sptemp2$dec,
-  mtry = 3,
-  num.trees = 2000
-)
-
-dat_sptemp_test2 <- simulate_ab(
-  response = "spatio-temporal",
-  t_x = seq(0, 9.5, 0.5)
-)
-pred_sim2 <- predict(res_sim_sptemp2, data = dat_sptemp_test2$dec)
-dat_sptemp_test2$dec$pred <- pred_sim2$predictions
-
-# Variable temporal
-res_sim_tempvar1 <- ranger(
-  abundance ~
-    env_temporal +
-      env_rand1_temporal +
-      env_rand2_temporal +
-      env_rand3_temporal +
-      env_spatial +
-      env_rand1_spatial +
-      env_rand2_spatial +
-      env_rand3_spatial,
-  data = dat_tempvar1$dec,
-  mtry = 6,
-  num.trees = 2000
-)
-
-dat_tempvar_test1 <- simulate_ab(response = "temporal-variable")
-pred_sim1 <- predict(res_sim_tempvar1, data = dat_tempvar_test1$dec)
-dat_tempvar_test1$dec$pred <- pred_sim1$predictions
-
-res_sim_tempvar2 <- ranger(
-  abundance ~
-    env_temporal +
-      env_rand1_temporal +
-      env_rand2_temporal +
-      env_rand3_temporal +
-      env_spatial +
-      env_rand1_spatial +
-      env_rand2_spatial +
-      env_rand3_spatial,
-  data = dat_tempvar2$dec,
-  mtry = 6,
-  num.trees = 2000
-)
-
-dat_tempvar_test2 <- simulate_ab(
-  response = "temporal-variable",
-  t_x = seq(0, 9.5, 0.5)
-)
-pred_sim2 <- predict(res_sim_tempvar2, data = dat_tempvar_test2$dec)
-dat_tempvar_test2$dec$pred <- pred_sim2$predictions
+pred_uneven <- foreach(i = 1:length(res_test_uneven)) %do%
+  {
+    predict(res_test_uneven[[i]], data = dat_test_uneven[[i]]$raw)
+  }
+for (i in 1:length(dat_test_uneven)) {
+  dat_test_uneven[[i]]$raw$pred <- pred_uneven[[i]]$predictions
+}
 
 # Plots
 plot_test <- function(dat) {
@@ -670,14 +479,134 @@ plot_test <- function(dat) {
     )
 }
 
-plot_test(dat_sp_test1$raw)
-plot_test(dat_sp_test2$raw)
+g_pred_equal <- foreach(i = 1:length(dat_test_equal)) %do%
+  {
+    plot_test(dat_test_equal[[i]]$raw)
+  }
+names(g_pred_equal) <- scenarios
 
-plot_test(dat_temp_test1$raw)
-plot_test(dat_temp_test2$raw)
+g_pred_uneven <- foreach(i = 1:length(dat_test_uneven)) %do%
+  {
+    plot_test(dat_test_uneven[[i]]$raw)
+  }
+names(g_pred_uneven) <- scenarios
 
-plot_test(dat_sptemp_test1$dec)
-plot_test(dat_sptemp_test2$dec)
 
-plot_test(dat_tempvar_test1$dec)
-plot_test(dat_tempvar_test2$dec)
+# Plots -------------------------------------------------------------------
+
+# Simulated environment
+## Equal trends in space and time
+s_x <- 1:20
+m_x <- foreach(i = 0:19, .combine = "rbind") %do%
+  {
+    s_x + i
+  }
+m_x2 <- t(apply(m_x, c(1, 2), function(x) x + rnorm(1, 0, 1)))
+
+m_x <- as.data.frame(m_x)
+colnames(m_x) <- 1:20
+m_x$site <- 1:20
+
+m_x <- m_x %>%
+  pivot_longer(-site, names_to = "time", values_to = "x")
+m_x$time <- as.numeric(m_x$time)
+m_x$x <- as.numeric(m_x$x)
+
+ggplot(data = m_x) +
+  geom_tile(
+    aes(y = site, x = time, fill = x),
+    #hjust = 0,
+    #vjust = 0,
+    color = "black",
+    linewidth = 0.5
+  ) +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_blank()
+  ) +
+  scale_fill_viridis_c(limits = c(-1, 40))
+ggsave("fig11.pdf", width = 180, height = 180, units = "mm", dpi = 600)
+
+m_x2 <- as.data.frame(m_x2)
+colnames(m_x2) <- 1:20
+m_x2$site <- 1:20
+
+m_x2 <- m_x2 %>%
+  pivot_longer(-site, names_to = "time", values_to = "x")
+m_x2$time <- as.numeric(m_x2$time)
+m_x2$x <- as.numeric(m_x2$x)
+
+ggplot(data = m_x2) +
+  geom_tile(
+    aes(y = site, x = time, fill = x),
+    #hjust = 0,
+    #vjust = 0,
+    color = "black",
+    linewidth = 0.5
+  ) +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_blank()
+  ) +
+  scale_fill_viridis_c(limits = c(-1, 40))
+
+ggsave("fig12.pdf", width = 180, height = 180, units = "mm", dpi = 600)
+
+## Weaker trends across time
+t_x <- seq(0, 9.5, 0.5)
+m_x3 <- foreach(i = 1:20, .combine = "rbind") %do%
+  {
+    s_x + t_x[i]
+  }
+m_x3 <- t(m_x3)
+m_x4 <- apply(m_x3, c(1, 2), function(x) x + rnorm(1, 0, 1))
+
+m_x3 <- as.data.frame(m_x3)
+colnames(m_x3) <- 1:20
+m_x3$site <- 1:20
+
+m_x3 <- m_x3 %>%
+  pivot_longer(-site, names_to = "time", values_to = "x")
+m_x3$time <- as.numeric(m_x3$time)
+m_x3$x <- as.numeric(m_x3$x)
+
+ggplot(data = m_x3) +
+  geom_tile(
+    aes(y = site, x = time, fill = x),
+    #hjust = 0,
+    #vjust = 0,
+    color = "black",
+    linewidth = 0.5
+  ) +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_blank()
+  ) +
+  scale_fill_viridis_c(limits = c(-1, 40))
+
+ggsave("fig13.pdf", width = 180, height = 180, units = "mm", dpi = 600)
+
+m_x4 <- as.data.frame(m_x4)
+colnames(m_x4) <- 1:20
+m_x4$site <- 1:20
+
+m_x4 <- m_x4 %>%
+  pivot_longer(-site, names_to = "time", values_to = "x")
+m_x4$time <- as.numeric(m_x4$time)
+m_x4$x <- as.numeric(m_x4$x)
+
+ggplot(data = m_x4) +
+  geom_tile(
+    aes(y = site, x = time, fill = x),
+    #hjust = 0,
+    #vjust = 0,
+    color = "black",
+    linewidth = 0.5
+  ) +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_blank()
+  ) +
+  scale_fill_viridis_c(limits = c(-1, 40))
+
+ggsave("fig14.pdf", width = 180, height = 180, units = "mm", dpi = 600)
